7e2c851f246e6cb3940a6dea5b280b9cd880c3c8
Victor Lee <myleevictor@gmail.com>
2017-10-31 21:43:36 +0800

    setup initial UI and tableviewCells

--- CoreDataApp/AppDelegate.swift
+++ CoreDataApp/AppDelegate.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+// Create custom Navigation Controller class to render the top status bar style to show light content
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +23,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let companiesController = ViewController()
+        
+        // navController should be subclass of the customer Navigation Controller
+        let navController = CustomNavigationController(rootViewController: companiesController)
+        window?.rootViewController = navController
+        
         return true
     }
 
--- CoreDataApp/ViewController.swift
+++ CoreDataApp/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var companies: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupNavigationStyle()
+
+        companies.append("hello")
+        companies.append("new hello")
+        companies.append("again hi there")
+        companies.append("hey hey hey")
+        
+        view.backgroundColor = .white
+        tableView.backgroundColor = UIColor.rgb(r: 9, g: 45, b: 64)
+//        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func handleAddCompany(){
+        print("Adding company")
+    }
+    
+    func setupNavigationStyle(){
+        navigationController?.navigationBar.isTranslucent = false
+        
+        let lightRed = UIColor.rgb(r: 247, g: 66, b: 82)
+        navigationController?.navigationBar.barTintColor = lightRed
+        
+        navigationItem.title = "Companies"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        //titleTextAttributes for small title at top of navbar
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
+        //LargeTitleTextAttributes for large title when static
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+
+        cell.textLabel?.text = companies[indexPath.row]
+        
+        return cell
     }
 
+    
+}
 
+extension UIColor {
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
 }
 
